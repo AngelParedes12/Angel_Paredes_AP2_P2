714fd6f3b7a7bae3be175164a64e445ccab57c34
AngelParedes12 <angel_paredes@ucne.edu.do>
2025-10-29 20:02:02 -0400

    Segundo commit

--- app/src/main/java/edu/ucne/angel_paredes_ap2_p2/MainActivity.kt
+++ app/src/main/java/edu/ucne/angel_paredes_ap2_p2/MainActivity.kt
@@ -11,8 +11,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.AndroidEntryPoint
 import edu.ucne.angel_paredes_ap2_p2.ui.theme.Angel_Paredes_AP2_P2Theme
-
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
