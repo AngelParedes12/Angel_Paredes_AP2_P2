471f4a71470a99c40389cedaf6b0b913ef3d3d33
AngelParedes12 <angel_paredes@ucne.edu.do>
2025-11-12 15:45:42 -0400

    Eliminando componentes, de la pantalla que no se iba a utilizar para la entrega de este examen

--- app/src/main/java/edu/ucne/angel_paredes_ap2_p2/Presentation/Edit/EditGastoUiEvent.kt
+++
@@ -1,9 +0,0 @@
-package edu.ucne.angel_paredes_ap2_p2.Presentation.Edit
-
-interface EditGastoUiEvent {
-    data class descripcionChanged(val v: String): EditGastoUiEvent
-    data class montoChanged(val v: String): EditGastoUiEvent
-    data class fechaChanged(val v: String): EditGastoUiEvent
-    data class load(val id: Int?): EditGastoUiEvent
-    data object save: EditGastoUiEvent
-}
