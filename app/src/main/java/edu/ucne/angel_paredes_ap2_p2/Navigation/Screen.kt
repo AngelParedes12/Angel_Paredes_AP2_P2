5b730fc6a5b71026f50ab125fadd2371b920a777
AngelParedes12 <angel_paredes@ucne.edu.do>
2025-11-12 15:47:11 -0400

    feat: Se eliminaron archivos de gastos
    Se eliminaron los siguientes archivos relacionados con la funcionalidad de gastos:
    *   `ListGastoScreen.kt`
    *   `ListGastoUiState.kt`
    *   `ListGastoViewModel.kt`
    *   `PutGastoUseCase.kt`
    *   `Screen.kt` (que contenía las rutas de navegación para gastos)

--- app/src/main/java/edu/ucne/angel_paredes_ap2_p2/Navigation/Screen.kt
+++
@@ -1,9 +0,0 @@
-package edu.ucne.angel_paredes_ap2_p2.Navigation
-
-sealed class Screen(val route: String) {
-    data object ListGasto : Screen("list_gasto_screen")
-    data object EditGasto : Screen("edit_gasto_screen/{gastoId}") {
-        const val ARG = "gastoId"
-        fun createRoute(id: Int) = "edit_gasto_screen/$id"
-    }
-}
--- app/src/main/java/edu/ucne/angel_paredes_ap2_p2/Presentation/List/ListGastoUiState.kt
+++
@@ -1,9 +0,0 @@
-package edu.ucne.angel_paredes_ap2_p2.Presentation.List
-
-import edu.ucne.angel_paredes_ap2_p2.Di.Gasto
-
-
-data class ListGastoUiState(
-    val gastos: List<Gasto> = emptyList(),
-    val isLoading: Boolean = false
-)
